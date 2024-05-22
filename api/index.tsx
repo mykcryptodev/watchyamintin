46e3d8f0cc3e0615d50d3f05daece1edcb913be5
Myk <mykcryptodev@gmail.com>
2024-05-22 00:40:48 -0400

    no conflict

--- api/index.tsx
+++ api/index.tsx
@@ -1,7 +1,6 @@
 import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
-import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
 import { Box, Heading, Text, Image, VStack, vars, HStack } from "../ui.js"
 import { toTokens } from 'thirdweb'
